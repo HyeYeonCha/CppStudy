e1fd5cae9aa0d5c4df9172d7c174fbf8074f7860
HyeYeonCha <gpdus335@naver.com>
2019-12-02 10:13:34 +0900

    배열 테스트

--- source/ConsoleApplication1/1202.cpp
+++ source/ConsoleApplication1/1202.cpp
@@ -3,23 +3,15 @@
 
 
 int main(void) {
-	int val1, val2;
-	int result = 0;
-	std::cout << "두 개의 숫자 입력";
-	std::cin >> val1 >> val2;
-	
-	if (val1 < val2) {
-		for (int i = val1+1; i < val2; i++)
-		{
-			result += i;
-		}
-	}
-	else {
-		for (int i = val2+1; i < val1; i++)
-		{
-			result += i;
-		}
-	}
+	char name[100];
+	char lang[200];
 
-	std::cout << "두 수 사이의 정수 합: " << result << std::endl;
+	std::cout << "이름은 무엇입니까?";
+	std::cin >> name;
+	std::cout << "좋아하는 언어는 무엇?";
+	std::cin >> lang;
+
+	std::cout << "내 이름은 " << name << "입니다.";
+	std::cout << "좋아하는 언어는 " << lang << "입니다. " << std::endl;
+	return 0;
 }
