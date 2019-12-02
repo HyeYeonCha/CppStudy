0d10b93ccc5f72a1e294116e2255a56f1aab0749
HyeYeonCha <gpdus335@naver.com>
2019-12-02 13:00:34 +0900

    01-02 문제 풀이

--- source/ConsoleApplication1/1202.cpp
+++ source/ConsoleApplication1/1202.cpp
@@ -1,65 +1,44 @@
 #include "pch.h"
 #include <iostream>
 
+int swap(int * a, int * b) {
+	int tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+
+	return *a, *b;
+}
+
+char swap(char *a, char *b) {
+	char tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+
+	return *a, *b;
+}
+
+double swap(double *a, double *b) {
+	double tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+
+	return *a, *b;
+}
 
 int main(void) {
 	
-	//// 1
-	//int input;
-	//int result = 0;
+	int num1 = 20, num2 = 30;
+	swap(&num1, &num2);
+	std::cout << num1 << ' ' << num2 << std::endl;
 
-	//for (int i = 0; i < 5; i++)
-	//{
-	//	std::cout << i << "번째 정수 입력 : ";
-	//	std::cin >> input;
-	//	result += input;
-	//}
+	char ch1 = 'A', ch2 = 'Z';
+	swap(&ch1, &ch2);
+	std::cout << ch1 << ' ' << ch2 << std::endl;
 
-	//std::cout << "합계 : " << result;
-
-	//// 2
-	//char name[10];
-	//char phone[15];
-
-	//std::cout << "이름은? : ";
-	//std::cin >> name;
-
-	//std::cout << "전화번호는? : ";
-	//std::cin >> phone;
-
-	//std::cout << "이름은 " << name << " 전화번호는 " << phone;
-
-	////3
-	//int val;
-	//std::cout << "몇 단? : ";
-	//std::cin >> val;
-
-	//for (int i = 0; i < 10; i++)
-	//{
-	//	std::cout << val << " * " << i << " = " << val * i << std::endl;
-	//}
-
-	//4
-
-	int val = 0; 
-	double hap = 0;
-	
-	while (1)
-	{
-
-		std::cout << "판매 금액을 만원 단위로 입력 (-1 to end): ";
-		std::cin >> val;
-
-		if (val == -1) {
-
-			break;
-		}
-
-		hap = 50 + val * 0.12;
-		std::cout << "이번 달 급여 : " << hap << std::endl;
-
-	}
-	std::cout << "프로그램을 종료합니다.";
-	return 0;
-
+	double dbl1 = 1.111, dbl2 = 5.555;
+	swap(&dbl1, &dbl2);
+	std::cout << dbl1 << ' ' << dbl2 << std::endl;
 }
