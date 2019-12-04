3e50d1751304d2dffecbc25e976c6f49639989a5
HyeYeonCha <gpdus335@naver.com>
2019-12-04 11:26:46 +0900

    02-3 문제 풀이

--- source/ConsoleApplication1/1202Main.cpp
+++ source/ConsoleApplication1/1202Main.cpp
@@ -4,18 +4,38 @@
 using namespace std;
 using namespace aa;
 
+typedef struct __Point {
+	int xpos;
+	int ypos;
+}Point;
+
+Point& PntAdder(const Point &p1, const Point &p2) {
+	
+	Point *ppt = new Point;
+
+	ppt->xpos = p1.xpos + p2.xpos;
+	ppt->ypos = p1.ypos + p2.ypos;
+	
+	return *ppt;
+}
 
 int main(void) {
 
-	const int num = 12;
-	const int* ptr = &num;
+	Point *p1 = new Point;
+	p1->xpos = 1;
+	p1->ypos = 2;
 
-	const int &ref = *ptr;
-	//const int *(&ref) = ptr;
+	Point *p2 = new Point;
+	p2->xpos = 2;
+	p2->ypos = 1;
 
-	cout << *ptr << endl;
-	cout << *(&ref) << endl;
-	//cout << *ref << endl;
+	Point &ref = PntAdder(*p1, *p2);
+
+	cout << "(" << ref.xpos << "," << ref.ypos << ")" << endl;
+
+	delete p1;
+	delete p2;
+	delete &ref;
 
 	return 0;
 }
