adb41c88ac3bb58e2eace4fe4bcb6b3fe5544c50
HyeYeonCha <gpdus335@naver.com>
2019-12-04 14:44:09 +0900

    03-1 문제풀이

--- source/ConsoleApplication1/1202Main.cpp
+++ source/ConsoleApplication1/1202Main.cpp
@@ -5,19 +5,36 @@
 
 using namespace std;
 
-void Random(void) {
-	srand(time(NULL));
+struct Point {
+	int xpos;
+	int ypos;
 
-	for (int i = 0; i < 5; i++)
-	{
-		cout << "Random Number " << i + 1 << " : " << rand() % 100 << endl;
+	void MovePos(int x, int y) {
+		xpos += x;
+		ypos += y;
 	}
 
-}
+	void AddPoint(const Point &pos) {
+		xpos += pos.xpos;
+		ypos += pos.ypos;
+	}
+
+	void ShowPosition() {
+		cout << "(" << xpos << "," << ypos << ")" << endl;
+	}
+
+};
 
 int main(void) {
 
-	Random();
+	Point pos1 = { 12, 4 };
+	Point pos2 = { 20, 30 };
+
+	pos1.MovePos(-7, 10);
+	pos1.ShowPosition();
+
+	pos1.AddPoint(pos2);
+	pos1.ShowPosition();
 	
 	return 0;
 }
